2c27d1dfc90971fb71794cf9d530cf3cca2eeba6
VegetaCK <91288502+VegetaCK@users.noreply.github.com>
2025-04-24 13:42:40 +0300

    Update parrot.py

--- parrot.py
+++ parrot.py
@@ -1,6 +1,8 @@
 while True:
-    phrase = input("Скажи что нибудь")
-  if phrase.lower() in ["выход", "exit", "quit"]:
-    print ("Пока")
-    break
-print("Попугай сказал:", phrase)
+    phrase = input("Скажи что-нибудь: ")
+    
+    if phrase.lower() in ["выход", "exit", "quit"]:
+        print("Пока")
+        break
+
+    print("Попугай сказал:", phrase)
